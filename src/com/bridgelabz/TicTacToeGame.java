36c45c21a116698abe0b7e82d29b9d9449ee3be9
sreelipta <sreelipta65@gmail.com>
2021-08-11 15:48:16 +0530

    Added: user can move to a desired location in board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -20,6 +20,8 @@ public class TicTacToeGame {
         choosingXorO();
         //calling method to show the current board
         showBoard();
+        //calling method to make a move to a desired location in board by the user
+        userPlay();
     }
 
     /**
@@ -63,15 +65,71 @@ public class TicTacToeGame {
      * creating method for show current board
      */
     public static void showBoard() {
-        // 0th index ignored
-        System.out.println("**Tic Tac Toe Game Board **\n");
-        System.out.println("-------------");
-        System.out.println("| "+board[1]+" | "+board[2]+" | "+board[3]+" |\n ");
-        System.out.println("-------------");
-        System.out.println("| "+board[4]+" | "+board[5]+" | "+board[6]+" |\n ");
-        System.out.println("-------------");
-        System.out.println("| "+board[7]+" | "+board[8]+" | "+board[9]+" |\n ");
-        System.out.println("-------------");
+        // 0th index
+        System.out.println("TIC TAC TOE GAME BOARD");
+        System.out.println(board[1] + " |" + board[2] + " |" + board[3]);
+        System.out.println("--+--+--");
+        System.out.println(board[4] + " |" + board[5] + " |" + board[6]);
+        System.out.println("--+--+--");
+        System.out.println(board[7] + " |" + board[8] + " |" + board[9]);
+
+    }
+
+    // method makes move to location given by user
+    static void userPlay() {
+        System.out.println("Enter board index(1-9) to set your game move ");
+        Scanner sc = new Scanner(System.in);
+        int userMove = sc.nextInt();
+        switch (userMove) {
+            case 1:
+                if (board[1] == ' ') {
+                    board[1] = userLetter;
+                }
+                break;
+            case 2:
+                if (board[2] == ' ') {
+                    board[2] = userLetter;
+                }
+                break;
+            case 3:
+                if (board[3] == ' ') {
+                    board[3] = userLetter;
+                }
+                break;
+            case 4:
+                if (board[4] == ' ') {
+                    board[4] = userLetter;
+                }
+                break;
+            case 5:
+                if (board[5] == ' ') {
+                    board[5] = userLetter;
+                }
+                break;
+            case 6:
+                if (board[6] == ' ') {
+                    board[6] = userLetter;
+                }
+                break;
+            case 7:
+                if (board[7] == ' ') {
+                    board[7] = userLetter;
+                }
+                break;
+            case 8:
+                if (board[8] == ' ') {
+                    board[8] = userLetter;
+                }
+                break;
+            case 9:
+                if (board[9] == ' ') {
+                    board[9] = userLetter;
+                }
+                break;
+            default:
+                System.out.println("Invalid Move");
+        }
+        showBoard();
     }
 }
 
